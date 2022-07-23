30aa2fdae040a9054d025785c135bf9be30b943c
JoseJaraba <josem.jaraba@hotmail.com>
2022-07-23 18:05:27 -0500

    Shopping cart

--- src/index.js
+++ src/index.js
@@ -4,10 +4,27 @@ const popUpMenu = document.querySelector('.desktop-menu ');
 const burgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCartMenu = document.querySelector('.product-detail');
+
+
 navbarEmail.addEventListener('click', () => {
+    if(!shoppingCartMenu.classList.contains('inactive')){
+        shoppingCartMenu.classList.add('inactive');
+    }
     popUpMenu.classList.toggle('inactive');
 });
 
 burgerMenu.addEventListener('click', () => {
-    mobileMenu.classList.toggle('inactive')
+    if(!shoppingCartMenu.classList.contains('inactive')){
+        shoppingCartMenu.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+});
+
+shoppingCartIcon.addEventListener('click', () => {
+    if(!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive');
+    }
+    shoppingCartMenu.classList.toggle('inactive');
 });

ecdb96bfa17729ae7f6ef605de76da0a9c4b2f1f
JoseJaraba <josem.jaraba@hotmail.com>
2022-07-23 17:15:58 -0500

    Merging mobile menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const popUpMenu = document.querySelector('.desktop-menu ');
 
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 navbarEmail.addEventListener('click', () => {
     popUpMenu.classList.toggle('inactive');
+});
+
+burgerMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive')
 });
